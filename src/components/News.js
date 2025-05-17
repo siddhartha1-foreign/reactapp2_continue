333556347356a94acb016ea8ab3f0a30df3fc166
siddhartha <siddhartha.foreign@gmail.com>
2025-05-17 23:00:30 +0530

    3rd commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,58 +1,96 @@
-import React, { Component } from 'react';
-import NewsItem from './NewsItem';
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles = [
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ] 
-   constructor(){
+  constructor() {
     super();
     console.log("Hello I am a constructor from news component");
-    this.state={
-       articles : this.articles,
-       loading: false
-    }
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
   }
-//runs after render
-  componentDidMount()
-  {
-    console.log("cdm");
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=b5e12497ba604abfbb9f618caa1f68f1&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
+
+  handlePrevClick = async () => {
+   
+      let newPage = this.state.page - 1;
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=b5e12497ba604abfbb9f618caa1f68f1&page=${newPage}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: newPage,
+        articles: parsedData.articles,
+      });
+    
+  };
+
+  handleNextClick = async () => {
+     if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+    let newPage = this.state.page + 1;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=b5e12497ba604abfbb9f618caa1f68f1&page=${newPage}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: newPage,
+      articles: parsedData.articles,
+    });
+  }
+  };
+
   render() {
     console.log("render");
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
-        <div className="row" >
-        {this.state.articles.map((element)=>{
-           return <div className="col-md-4" key={element.url}><NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/></div>
-           
+        <h2 className="text-center">NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
           })}
-          </div>
-       
-        
-        {/* <NewsItem title="myTitle" description="mydesc" />
-        <NewsItem title="myTitle" description="mydesc" />
-        <NewsItem title="myTitle" description="mydesc" /> */}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            disabled={this.state.page <= 1}
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://nbcsports.brightspotcdn.com/dims4/default/7f4e648/2147483647/strip/true/crop/5421x3049+0+282/resize/1440x810!/quality/90/?url=https%3A%2F%2Fnbc-sports-production-nbc-sports.s3.us-east-1.amazonaws.com%2Fbrightspot%2F4d%2Fa2%2F56162fa24e459cc877fec7aeacf8%2Fhttps-delivery-gettyimages.com%2Fdownloads%2F2194482243":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Go somewhere</a>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark btn-primary">Go somewhere</a>
           </div>
         </div>
       </div>
